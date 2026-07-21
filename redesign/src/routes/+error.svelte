2f563ae420e7e08429568f3956ff91fb445b5e4e
asteinhart <asteinhart3@gmail.com>
2026-07-20 22:13:50 -0400

    error page

---
+++ redesign/src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Spacer from '$lib/components/Spacer.svelte';
+	import Nav from '$lib/components/Nav.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<div class="container">
+	<Nav all={true} />
+	<Spacer />
+
+	This ain't a page, this is an error. But you can try going back to the <a href="/">home page</a>
+	or checking out some of my
+	<a href="/projects">projects</a>
+	or <a href="/blog">blog posts</a>.
+</div>
