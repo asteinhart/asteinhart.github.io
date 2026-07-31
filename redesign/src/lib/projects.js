899c93210230064cf48e4306bb630e3cee05f7e8
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2026-07-30 23:14:25 -0400

    images

--- redesign/src/lib/projects.js
+++ redesign/src/lib/projects.js
@@ -36,7 +36,7 @@ export const PROJECTS = {
 		url: 'https://www.cfr.org/article/visualizing-2026-five-foreign-policy-trends-watch',
 		description:
 			"What trends are Council on Foreign Relations foreign policy experts watching in 2026? Read about the race for critical minerals, the possible end of arms control, and the rise of the ‘Electrostate' (plus some visuals I contributed).",
-		img: 'static/style/images/viz2026.webp',
+		img: 'src/lib/assets/project-imgs/viz2026.webp',
 		tags: ['Graphics', 'Design']
 	},
 	opendatle: {
@@ -44,7 +44,7 @@ export const PROJECTS = {
 		url: 'https://www.open-datle.com',
 		description:
 			'A daily game to learn more about your city using open data. Each day you get shown a new data set and either have to draw the trend or rank the boroughs.',
-		img: 'static/style/images/open-datle.webp',
+		img: 'src/lib/assets/project-imgs/open-datle.webp',
 		tags: ['Backend', 'Frontend', 'Design']
 	},
 	great_aid_recession: {
@@ -52,7 +52,7 @@ export const PROJECTS = {
 		url: 'https://www.cfr.org/expert-brief/great-aid-recession-2025s-humanitarian-crash-nine-charts',
 		description:
 			'The world faces unresolved conflicts, growing climate crises, attacks on aid workers, two famines, and diminishing political will—along with significant aid cuts. Altogether, 2025 has earned a grim new superlative: the worst humanitarian year on record. Read the full article by Sam Vigersky with nine charts I helped create to illustrate the crisis.',
-		img: 'static/style/images/hum_aid.webp',
+		img: 'src/lib/assets/project-imgs/hum_aid.webp',
 		tags: ['Graphics', 'Design']
 	},
 	cta_bus_service: {
@@ -60,7 +60,7 @@ export const PROJECTS = {
 		url: 'https://www.chicagotribune.com/2024/12/30/cta-bus-service-analysis/?share=yarmeie3ytbainrsuh1l',
 		description:
 			'Worked on a team to produce the analysis and visualizations used in this story in the Chicago Tribune.',
-		img: 'static/style/images/cta.webp',
+		img: 'src/lib/assets/project-imgs/cta.webp',
 		tags: ['Data-Eng']
 	},
 	chainlink: {
@@ -68,7 +68,7 @@ export const PROJECTS = {
 		url: 'https://github.com/mansueto-institute/mi-chainlink',
 		description:
 			'A powerful, flexible Python package for entity resolution and record linkage. Enables users to load data from various sources, clean and standardize entity names and addresses, and create links between entities based on exact and fuzzy matching techniques.',
-		img: 'static/style/images/chainlink.png',
+		img: 'src/lib/assets/project-imgs/chainlink.png',
 		tags: ['Data-Eng']
 	},
 	thesunset: {
@@ -76,7 +76,7 @@ export const PROJECTS = {
 		url: 'https://thesunset.austinsteinhart.com',
 		description:
 			'Never miss the sunset again. An automated system that captures, scores, and displays the best sunset photo from the previous night using computer vision and a Raspberry Pi.',
-		img: 'static/style/images/thesunset.webp',
+		img: 'src/lib/assets/project-imgs/thesunset.webp',
 		tags: ['Backend', 'Frontend', 'Design']
 	},
 	who_owns_chicago: {
@@ -84,7 +84,7 @@ export const PROJECTS = {
 		url: 'https://whoownschi.miurban-dashboards.org/',
 		description:
 			'A web app that allows users to identify the owners of every parcel of land in Chicago and Cook County including an interactive map with 1.4M building parcels using MapLibre to allow research and journalists to explore corporate property ownership in Chicago.',
-		img: 'static/style/images/chicago.webp',
+		img: 'src/lib/assets/project-imgs/chicago.webp',
 		tags: ['Backend', 'Frontend', 'Mapping']
 	},
 	chicago_property_tax_explainer: {
@@ -92,7 +92,7 @@ export const PROJECTS = {
 		url: 'https://ptaxexplainer.miurban-dashboards.org/',
 		description:
 			'A web app that allows a homeowner in Chicago to explore their property tax bill, including how it is calculated, what the different components are, and how it has changed over time.',
-		img: 'static/style/images/tax_explain.webp',
+		img: 'src/lib/assets/project-imgs/tax_explain.webp',
 		tags: ['Backend', 'Frontend']
 	},
 	ai_in_gov: {
@@ -100,14 +100,14 @@ export const PROJECTS = {
 		url: 'ai-in-gov',
 		description:
 			'In 2023, the Biden Administration released the first ever Federal AI Use Case Inventory. See how the federal government is using AI.',
-		img: 'static/style/images/ai.webm',
+		img: 'src/lib/assets/project-imgs/ai.webm',
 		tags: ['Writing', 'Frontend', 'Design']
 	},
 	find_early_voting: {
 		title: 'Find a Chicago Early Voting Location',
 		url: 'https://chicagomaroon.com/l95h',
 		description: 'A map of all 56 Early Voting locations in Chicago.',
-		img: 'static/style/images/voting.png',
+		img: 'src/lib/assets/project-imgs/voting.png',
 		tags: ['Writing', 'Mapping']
 	},
 	ctastopwatch: {
@@ -115,7 +115,7 @@ export const PROJECTS = {
 		url: 'https://ctastopwatch.miurban-dashboards.org/',
 		description:
 			'Built a web app as part of a research project that analyzed a unique dataset of over 100 million real-time bus locations in partnership with the Chicago Tribune.',
-		img: 'static/style/images/sw.png',
+		img: 'src/lib/assets/project-imgs/sw.png',
 		tags: ['Backend', 'Frontend']
 	},
 	common_thread: {
@@ -123,7 +123,7 @@ export const PROJECTS = {
 		url: 'https://github.com/uchicago-capp-30320/CommonThread',
 		description:
 			'Currently in development, Common Thread is an open-source collaborative tool that enables users to turn stories into insights. Community-based organizations, journalists, researchers, and community members alike can use the tool to efficiently gather stories, aggregate them, and understand common threads to draw insights for action.',
-		img: 'static/style/images/ct.png',
+		img: 'src/lib/assets/project-imgs/ct.png',
 		tags: ['Backend', 'Frontend', 'Design']
 	},
 	university_faculty_donations: {
@@ -131,7 +131,7 @@ export const PROJECTS = {
 		url: 'https://chicagomaroon.com/41302/grey-city/university-faculty-arent-neutral-when-it-comes-to-political-donations/',
 		description:
 			'While the University has an official stance of institutional neutrality, faculty members lean decidedly Democratic. Won 1st place for Graphic Illustrations from the Illinois Collegiate Press Association.',
-		img: 'static/style/images/donations.png',
+		img: 'src/lib/assets/project-imgs/donations.png',
 		tags: ['Writing', 'Graphics']
 	},
 	kalven_report: {
@@ -139,14 +139,14 @@ export const PROJECTS = {
 		url: 'https://chicagomaroon.github.io/data-visualizations/2025/kalven-report-annotated/',
 		description:
 			'Inspired by an interview with journalist Jamie Kalven, son of Harry Kalven, Jr. of the Kalven Report, the Maroon annotates this oft-cited but little read document.',
-		img: 'static/style/images/annotating_4.webm',
+		img: 'src/lib/assets/project-imgs/annotating_4.webm',
 		tags: ['Frontend', 'Design']
 	},
 	content_we_love: {
 		title: 'Content We Love',
 		url: 'https://contentwelove.austinsteinhart.com',
 		description: 'Great reads, thoughtfully curated by your friends.',
-		img: 'static/style/images/content.png',
+		img: 'src/lib/assets/project-imgs/content.png',
 		tags: ['Backend', 'Frontend', 'Design']
 	},
 	current_uchicago_trustees_donations: {
@@ -154,14 +154,14 @@ export const PROJECTS = {
 		url: 'https://chicagomaroon.com/47691/grey-city/current-uchicago-trustees-donations-to-republicans-skyrocketed-in-last-two-years/',
 		description:
 			'UChicago trustees’ donations to Republican candidates increased considerably in the 2024 election cycle, according to Federal Election Commission records reviewed by the Maroon.',
-		img: 'static/style/images/trustees.png',
+		img: 'src/lib/assets/project-imgs/trustees.png',
 		tags: ['Graphics']
 	},
 	uchicago_common_data_set: {
 		title: 'Three Years Later, What Have We Learned from the UChicago Common Data Set?',
 		url: 'https://chicagomaroon.com/43910/data/three-years-later-what-have-we-learned-from-the-uchicago-common-data-set/',
 		description: 'The admission rate continues to drop as campus demographics have shifted.',
-		img: 'static/style/images/cds.png',
+		img: 'src/lib/assets/project-imgs/cds.png',
 		tags: ['Writing', 'Graphics']
 	},
 	my_uchicago_vulnerability: {
@@ -169,14 +169,14 @@ export const PROJECTS = {
 		url: 'https://chicagomaroon.com/46707/news/my-uchicago-vulnerability-could-have-exposed-students-class-schedules/',
 		description:
 			'The vulnerability, fixed last November, allowed access to current and previous class schedules for any student.',
-		img: 'static/style/images/breach.jpg',
+		img: 'src/lib/assets/project-imgs/breach.jpg',
 		tags: ['Writing']
 	},
 	my_reading_over_the_years: {
 		title: 'My Reading Over the Years',
 		url: 'https://austinsteinhart.com/reading',
 		description: 'Follow my reading journey from February 2016 to January 2024.',
-		img: 'static/style/images/reading.png',
+		img: 'src/lib/assets/project-imgs/reading.png',
 		tags: ['Writing', 'Frontend']
 	}
 };
