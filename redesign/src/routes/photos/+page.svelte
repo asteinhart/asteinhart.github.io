ac62e0bc5b7c202cec7bdebf28cdc373e9ce0a1d
asteinhart <asteinhart3@gmail.com>
2026-07-31 00:45:41 -0400

    photos v1

--- redesign/src/routes/photos/+page.svelte
+++ redesign/src/routes/photos/+page.svelte
@@ -2,6 +2,9 @@
 	import Nav from '$lib/components/Nav.svelte';
 	import Spacer from '$lib/components/Spacer.svelte';
 	import Seo from '$lib/components/Seo.svelte';
+	import { PHOTO_IDS } from '$lib/photos.js';
+
+	const src = (id) => `https://lh3.googleusercontent.com/d/${id}`;
 </script>
 
 <Seo title="Photos" description="A collection of photos by Austin Steinhart." />
@@ -9,7 +12,46 @@
 <div class="container">
 	<Nav photos={true} />
 	<Spacer />
-	<div class="spacer"></div>
-
-	coming soon...
 </div>
+
+<div class="container">
+	<Spacer />
+	<div class="gallery">
+		{#each PHOTO_IDS as id}
+			<img src={src(id)} alt="" loading="lazy" referrerpolicy="no-referrer" />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.full-line {
+		position: absolute;
+		width: 100%;
+		margin: 0;
+		z-index: 5;
+	}
+
+	.full-line.below {
+		margin-top: calc(var(--cell));
+	}
+
+	/* Masonry via CSS columns; gutters are one grid cell so photos line up
+	   with the page's background grid. */
+	.gallery {
+		column-count: 3;
+		column-gap: var(--cell);
+	}
+
+	.gallery img {
+		display: block;
+		width: 100%;
+		margin-bottom: var(--cell);
+		break-inside: avoid;
+	}
+
+	@media screen and (max-width: 768px) {
+		.gallery {
+			column-count: 1;
+		}
+	}
+</style>
