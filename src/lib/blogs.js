69f7415cd52480e15c9dce278646c1fa024a2c7e
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2026-08-09 10:15:36 -0400

    add workshops to blog

--- src/lib/blogs.js
+++ src/lib/blogs.js
@@ -6,6 +6,20 @@ export const BLOGS = {
 		description: 'A map of stories from the New York Times Metropolitan Diary column',
 		tags: ['mapping', 'design', 'data', 'frontend']
 	},
+	'hsy-data-viz': {
+		title: 'Data Viz and Visual Storytelling 101',
+		url: 'https://www.youtube.com/watch?v=m7UJfYywLU8',
+		date: '07/30/2026',
+		description: 'A workshop I led for Hack Your Summer covering ways to communicate with data, going beyond reports, maps, and charts to boost the effectiveness of storytelling by knowing your audience and being clear on the story you’re trying to tell.',
+		tags: ['guide', 'graphics', 'storytelling']
+	},
+	'nicar-good-design': {
+		title: 'NICAR 2026: From Good to Great Data Viz Design',
+		url: 'https://docs.google.com/presentation/d/19gNzG1OSH7jvYkrAB1m38F84BiCr71PkHszPX7uwDrM/edit?slide=id.p#slide=id.p',
+		date: '03/05/2026',
+		description: 'A presentation I co-led at the NICAR 2026 on how to take your data visualizations from good to great, with a focus on design principles and storytelling.',
+		tags: ['guide', 'graphics', 'storytelling']
+	},
 	scrolly: {
 		title: 'First scrollytelling',
 		url: 'https://austinsteinhart.com/docs/first-scrollytelly/',
